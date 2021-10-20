a5a12958a96a1dadff95148c95279872369c478c
DaZapata <davidzapata56@gmail.com>
2021-10-20 12:57:22 -0500

    destructuracion

--- 10-DOM_Mio/app.js
+++ 10-DOM_Mio/app.js
@@ -4,3 +4,7 @@ nuevoDiv.id = 'div-hola'
 nuevoDiv.setAttribute('title', 'hola mundo')
 nuevoDiv.appendChild(document.createTextNode('hello world'))
 console.log(nuevoDiv)
+
+
+//clave github
+//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
