2a165e5eb0a590cb3b88ff232c7b3d9d8ff81ffb
David Zapata <cristian.carrillo@controlt.com.co>
2023-06-17 10:18:05 -0500

    ajustes

--- playing-funciones/app.js
+++ playing-funciones/app.js
@@ -20,36 +20,38 @@ daysWeek.forEach((element) => {
   console.log(element);
 });
 
-const found = daysWeek.find((element) => element.includes("eves"));
-console.log(found);
+// const found = daysWeek.find((element) => element.includes("eves"));
+// console.log(found);
 
-//var dayWeek = prompt("Cual es tu dia se la semana preferido?").toLowerCase();
+var dayWeek = prompt("Cual es tu dia se la semana preferido?").toLowerCase();
 
-// function CheckDay(day) {
-//   switch (day) {
-//     case "lunes":
-//       return "perezoso";
-//       break;
-//     case "martes":
-//       return "abrumador";
-//       break;
-//     case "miercoles":
-//       return "moral";
-//       break;
-//     case "jueves":
-//       return "espectante";
-//       break;
-//     case "viernes":
-//       return "alegria";
-//       break;
-//     case "sabado":
-//       return "coronamos";
-//       break;
-//     default:
-//       "probando";
-//       break;
-//   }
-// }
+function CheckDay(day) {
+  switch (day) {
+    case "lunes":
+      return "perezoso";
+      break;
+    case "martes":
+      return "abrumador";
+      break;
+    case "miercoles":
+      return "moral";
+      break;
+    case "jueves":
+      return "espectante";
+      break;
+    case "viernes":
+      return "alegria";
+      break;
+    case "sabado":
+      return "coronamos";
+      break;
+    default:
+      "probando";
+      break;
+  }
+}
+
+console.log(CheckDay(dayWeek));
 
 // var response = CheckDay(dayWeek);
 // listEmotions.push(response);
