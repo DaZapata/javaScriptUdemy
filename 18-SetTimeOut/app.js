53fdfd58ee64e994b3a32271d34dc8d20388ca6d
David Zapata <davidzapata56@gmail.com>
2023-07-06 18:00:17 -0500

    promesas

--- 18-SetTimeOut/app.js
+++ 18-SetTimeOut/app.js
@@ -19,24 +19,98 @@
 
 // primero();
 
-const getUsers = () => {
-    setTimeout(() => {
-        const userIds = [101, 102, 103, 105, 104]
-        console.log(userIds);
+//setTimeOut
+// const getUsers = () => {
+//     setTimeout(() => {
+//         const userIds = [101, 102, 103, 105, 104]
+//         console.log(userIds);
 
-        setTimeout((id) => {
+//         setTimeout((id) => {
+//             const user = {
+//                 email: 'david@mail.com',
+//                 name: 'david'
+//             }
+//             console.log(`${id} = ${user.email} - ${user.name}`);
+
+//             setTimeout((idPermiso) => {
+//                 const permiso = ['admin', 'creador']
+//                 console.log(permiso);
+//             }, 1500, userIds[1])
+//         }, 1500, userIds[1])
+//     }, 1500)
+// }
+
+// getUsers()
+
+// //ejemplo promesas
+// let promesa = new Promise((resolve, reject) => {
+//     if (false){
+//         resolve('todo salio bien')
+//     } else {
+//         reject('hubo un error')
+//     }
+// })
+
+// promesa.then(response => {
+//     console.log('Response:' + response);
+// }).catch(error => {
+//     console.log('Error:' + error);
+// })
+
+// //ejemplo 2
+// var miPromesa = Promise.resolve('comida')
+// miPromesa.then(resp => console.log(resp))
+
+// //ejemplo 3
+// var miPromesa2 = new Promise(function(resolve, reject){
+//     setTimeout(() => resolve(4), 2000)
+// })
+
+// miPromesa2.then(resp => {
+//     resp += 5
+//     console.log(resp);
+// })
+
+//De callbacks a promises
+const getUsersIds = new Promise((response, reject) => {
+    setTimeout(() => {
+        response([101, 102, 103, 105])
+    }, 1500)
+})
+
+const getUser = userId => {
+    return new Promise((resolve, reject) => {
+        setTimeout(id => {
             const user = {
                 email: 'david@mail.com',
                 name: 'david'
             }
-            console.log(`${id} = ${user.email} - ${user.name}`);
-
-            setTimeout((idPermiso) => {
-                const permiso = ['admin', 'creador']
-                console.log(permiso);
-            }, 1500, userIds[1])
-        }, 1500, userIds[1])
-    }, 1500)
+            resolve(`${id} = ${user.email} - ${user.name}`);
+        }, 1500, userId)
+    })
 }
 
-getUsers()
+getPermisos = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(id => {
+            const permiso = ['admin', 'creador']
+            resolve(permiso)
+        }, 1500, id)
+    })
+}
+
+getUsersIds
+.then(Ids => {
+    console.log((Ids));
+    return getUser(Ids[3])
+})
+.then(user => {
+    console.log(user);
+    return getPermisos(user.id)
+})
+.then(permisos => {
+    console.log(permisos);
+})
+.catch(() => {
+    console.log('Error');
+})
